2d4dc67dace51e17223c9430762ef06d916981ba
Jon Milner <jonmilner@users.noreply.github.com>
2018-05-05 13:10:17 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,35 @@
 module.exports = {
-  plugins: [
-    require('postcss-easy-import'),
-    require('postcss-for'),
-    require('postcss-each'),
-    require('postcss-mixins'),
-    require('postcss-custom-selectors'),
-    require('postcss-simple-vars'),
-    require('postcss-color-function'),
-    require('postcss-apply'),
-    require('postcss-calc'),
-    require('cq-prolyfill/postcss-plugin'),
-    require('postcss-nested'),
-    require('postcss-atroot'),
-    require('postcss-assets')({
+  plugins: {
+    'postcss-easy-import': {},
+    'postcss-for': {},
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-custom-selectors': {},
+    'postcss-simple-vars': {},
+    'postcss-color-function': {},
+    'postcss-apply': {},
+    'postcss-calc': {},
+    'postcss-nested': {},
+    'postcss-atroot': {},
+    'postcss-assets': {
       basePath: 'src/',
       cachebuster: true,
       loadPaths: ['img/'],
       relative: true
-    }),
-    require('postcss-inline-svg')({
+    },
+    'postcss-inline-svg': {
       path: 'src'
-    }),
-    require('postcss-initial'),
-    require('postcss-pseudo-class-enter'),
-    require('postcss-pxtorem')({
+    },
+    'postcss-initial': {},
+    'postcss-pseudo-class-enter': {},
+    'postcss-pxtorem': {
       unitPrecision: 4,
       propWhiteList: [],
       replace: true,
       selectorBlackList: [],
       minPixelValue: 4
-    }),
-    require('autoprefixer')({
+    },
+    autoprefixer: {
       /* http://browserl.ist/?q=last+2+Chrome+versions%2C+last+2+Firefox+versions%2C+last+2+Safari+versions%2C+last+2+Edge+versions%2C+last+4+iOS+versions%2C+last+2+ChromeAndroid+versions */
       browsers: [
         'last 2 Chrome versions',
@@ -40,8 +39,8 @@ module.exports = {
         'last 4 iOS versions',
         'last 2 ChromeAndroid versions'
       ]
-    }),
-    require('cssnano')({
+    },
+    cssnano: {
       preset: [
         'default',
         {
@@ -51,9 +50,9 @@ module.exports = {
           normalizeUrl: false
         }
       ]
-    }),
-    require('postcss-reporter')({
+    },
+    'postcss-reporter': {
       clearMessages: true
-    })
-  ]
+    }
+  }
 };
