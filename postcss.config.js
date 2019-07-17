49dc70f32332304557b4a77b00529d2d85db1ec1
Jon Milner <jonmilner@users.noreply.github.com>
2019-07-17 15:05:24 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -6,18 +6,7 @@ module.exports = {
     "postcss-mixins": {},
     "postcss-custom-selectors": {},
     "postcss-nested": {},
-    "postcss-preset-env": {
-      autoprefixer: {
-        browsers: [
-          "last 2 Chrome versions",
-          "last 2 Firefox versions",
-          "last 2 Safari versions",
-          "last 2 Edge versions",
-          "last 4 iOS versions",
-          "last 2 ChromeAndroid versions",
-        ],
-      },
-    },
+    "postcss-preset-env": {},
     "postcss-pxtorem": {
       unitPrecision: 4,
       propWhiteList: [],
